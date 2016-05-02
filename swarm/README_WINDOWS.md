e35bbd71cb457209599604b91804f507165464d1
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-05-02 17:24:05 -0400

    on windows the parens didnt work, changed the sed command

--- swarm/README_WINDOWS.md
+++ swarm/README_WINDOWS.md
@@ -10,7 +10,7 @@ Install Docker Toolbox for Windows
 Install Vagrant for Windows
 [https://www.vagrantup.com/downloads.html](https://www.vagrantup.com/downloads.html)
 
-Install Cygwin for Windows (with Net/*ssh* components)
+Install Cygwin for Windows (with Net/openssh and Devel/git components)
 [https://www.cygwin.com/](https://www.cygwin.com/)
 
 
@@ -18,14 +18,12 @@ Install Cygwin for Windows (with Net/*ssh* components)
 
 ## How to install
 
-Open `Git Bash` in Windows
-```
-git clone -b swarm https://github.com/wallnerryan/volume-plugins-demo
-```
+> Note: if vagrant errs out downloading the box and you're on windows sever 2012, you may need to install `Microsoft Visual C++ 2010 SP1 Redistributable Package (x86)` [here](http://www.microsoft.com/en-us/download/details.aspx?id=8328) according to [this github comment](https://github.com/mitchellh/vagrant/issues/6754#issuecomment-168856576)
 
 Open `Cygwin64 Terminal` in Windows
 ```
-cd /cygdrive/d/Users/<USERNAME>/volume-plugins-demo/
+git clone -b swarm https://github.com/wallnerryan/volume-plugins-demo
+cd volume-plugins-demo
 vagrant up
 ```
 
--- swarm/ready-docker-for-swarm_win.sh
+++ swarm/ready-docker-for-swarm_win.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # To be used with Cygwin on Windows
 echo "Setting DOCKER_OPTS on node1"
-vagrant ssh node1 -c "sudo sed -ie 's@.*DOCKER_OPTS=.*@DOCKER_OPTS=\"-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock\"@' /etc/default/docker.io"
+vagrant ssh node1 -c "sudo sed -ie 's@\-\-dns 8.8.8.8 \-\-dns 8.8.4.4@\-H tcp\://0.0.0.0:2375 \-H unix\:///var/run/docker.sock@' /etc/default/docker.io"
+vagrant ssh node1 -c "sudo sed -ie 's@#DOCKER_OPTS@DOCKER_OPTS@' /etc/default/docker.io"
 echo "Restarting the Docker Daemon on node1"
 vagrant ssh node1 -c "sudo service docker.io restart"
 echo "Setting DOCKER_OPTS on node2"
-vagrant ssh node2 -c "sudo sed -ie 's@.*DOCKER_OPTS=.*@DOCKER_OPTS=\"-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock\"@' /etc/default/docker.io"
+vagrant ssh node2 -c "sudo sed -ie 's@\-\-dns 8.8.8.8 \-\-dns 8.8.4.4@\-H tcp\://0.0.0.0:2375 \-H unix\:///var/run/docker.sock@' /etc/default/docker.io"
+vagrant ssh node2 -c "sudo sed -ie 's@#DOCKER_OPTS@DOCKER_OPTS@' /etc/default/docker.io"
 # otherwise Swarm complains about duplicate ID. https://github.com/docker/swarm/issues/362
 echo "Restarting the Docker Daemon on node2"
 vagrant ssh node2 -c "sudo rm /etc/docker/key.json"
