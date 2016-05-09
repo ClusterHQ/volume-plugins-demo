3b2268f5231d9d2caa7fb3ca6e054d44da56b8c5
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-05-09 11:45:51 -0400

    control service is also an agent, so lets make sure to run the agent service on the control node for volumehub as well

--- add-volumehub.sh
+++ add-volumehub.sh
@@ -4,15 +4,23 @@
 # and navigating to https://volumehub.clusterhq.com/v1/token
 [ -z "$TOKEN" ] && echo "Need to set TOKEN for VolumeHub" && exit 1;
 
+# Control service setup
 vagrant ssh node1 -c "sudo \
   TARGET=control-service \
   TOKEN=${TOKEN} \
   sh -c 'curl -ssL https://get-volumehub.clusterhq.com/ |sh'"
 
+# Need to run this once, lets run it on node2
 vagrant ssh node2 -c "sudo \
   TARGET=agent-node \
   RUN_FLOCKER_AGENT_HERE=1 \
   TOKEN=${TOKEN} \
   sh -c 'curl -ssL https://get-volumehub.clusterhq.com/ |sh'"
+
+# Control node is also an agent node.
+vagrant ssh node1 -c "sudo \
+  TARGET=agent-node \
+  TOKEN=${TOKEN} \
+  sh -c 'curl -ssL https://get-volumehub.clusterhq.com/ |sh'
 
 echo "Done! Go to https://volumehub.clusterhq.com/"
